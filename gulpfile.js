32fb10542bf8d6c8af200a91a31a3fae1e228c7a
Adam Zmenak <adam@dinteractive.ca>
2015-01-17 08:36:22 -0500

    Setup SASS and Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
@@ -7,9 +9,60 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 
+var watchify   = require('watchify');
+var browserify = require('browserify');
+var ngAnnotate = require('browserify-ngannotate');
+var notify     = require('gulp-notify');
+var source     = require('vinyl-source-stream');
+var browserSync  = require('browser-sync');
+
 var paths = {
   sass: ['./scss/**/*.scss']
 };
+
+var handleErrors = function(error) {
+  var args = Array.prototype.slice.call(arguments);
+
+  // Send error to notification center with gulp-notify
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+
+  // Keep gulp from hanging on this task
+  this.emit('end');
+}
+
+function buildScript(file) {
+  var bundler = browserify({
+    entries: [
+      './www/js/app.js',
+      './www/js/controllers.js'
+    ],
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  }, watchify.args);
+
+  bundler = watchify(bundler);
+  bundler.on('update', function() {
+    rebundle();
+  });
+
+  bundler.transform(ngAnnotate);
+
+  function rebundle() {
+    var stream = bundler.bundle();
+
+    gutil.log('Rebundle...');
+
+    return stream.on('error', handleErrors)
+      .pipe(source(file))
+      .pipe(gulp.dest('./www/js'))
+      .pipe(browserSync.reload({ stream: true, once: true }));
+  }
+  return rebundle();
+}
 
 gulp.task('default', ['sass']);
 
@@ -27,6 +80,7 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  
 });
 
 gulp.task('install', ['git-check'], function() {
@@ -34,6 +88,10 @@ gulp.task('install', ['git-check'], function() {
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
+});
+
+gulp.task('browserify', function() {
+  return buildScript('main.js');
 });
 
 gulp.task('git-check', function(done) {
---
+++ www/js/main.js
@@ -0,0 +1,86 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./www/js/app.js":[function(require,module,exports){
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(["$ionicPlatform", function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+}])
+
+.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+    .state('app.categories', {
+      url: "/categories",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/categories.html",
+          controller: 'CategoriesCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: "/category/:catId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/category.html",
+        controller: 'CategoryCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/categories');
+}]);
+
+},{}],"./www/js/controllers.js":[function(require,module,exports){
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', ["$scope", "$ionicModal", "$timeout", function($scope, $ionicModal, $timeout) {
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+}])
+
+.controller('CategoriesCtrl', ["$scope", function($scope) {
+  $scope.categories = [
+    { title: 'Image Categorization', id: 1 },
+    { title: 'Information Search', id: 2 },
+    { title: 'Image Filtering', id: 3 },
+    { title: 'Emotion Rating', id: 4 },
+    { title: 'Image Tagging', id: 5 },
+    { title: 'Transcription', id: 6 }
+  ];
+}])
+
+.controller('CategoryCtrl', ["$scope", "$stateParams", function($scope, $stateParams) {
+  $scope.pid = $stateParams['catId']
+}]);
+
+},{}]},{},["./www/js/app.js","./www/js/controllers.js"]);

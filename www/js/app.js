0d88004506dfb1b63acd5fa45ea0d566415c9743
Adam Zmenak <adam@dinteractive.ca>
2015-01-17 08:58:58 -0500

    Add firebase

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,11 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+
+firebase = require('firebase');
+angularfire = require('angularfire');
+
+angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
